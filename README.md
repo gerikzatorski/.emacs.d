0aec00d191445cfcd5ce6620d725a5ba0d98f319
Gerik Zatorski <gerikzatorski@gmail.com>
2017-07-17 18:56:55 -0500

    readded all the basics I like

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # .emacs.d
+
+## Packages to Consider:
+
+* idomenu
+* yasnippet
+* irony
+* jedi (is there an all around autocomplete?)
+* keychord
--- init.el
+++ init.el
@@ -1,1 +1,171 @@
-;; init.el
+;; -*- Mode: Emacs-Lisp -*-
+
+;; Filename: init.el
+;; Description: Gerik Zatorski's emacs configuration
+;; Author: Gerik Zatorski
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basics
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; no splash screen 
+(setq inhibit-startup-message t)
+
+;; Set path to dependencies
+(setq settings-dir (expand-file-name "settings" user-emacs-directory))
+
+;; Setup load path
+(add-to-list 'load-path settings-dir)
+
+;; handle other .el files
+;;(add-to-list 'load-path' "~/.emacs.d/functions.el")
+
+;; replaces region when typing
+(pending-delete-mode t)
+
+;; store all backup and autosave files in the system's temp dir
+;; https://www.emacswiki.org/emacs/BackupDirectory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Bind imenu
+(global-set-key (kbd "M-i") 'imenu)
+
+;; Bind common commands to function keys
+(global-set-key [f1] 'help-command)
+(global-set-key [f2] 'undo)
+(global-set-key [f7] 'save-buffer)
+
+;; Make F8 be "start macro", F9 be "end macro", F10 be "execute macro"
+(global-set-key [f8] 'start-kbd-macro)
+(global-set-key [f9] 'end-kbd-macro)
+(global-set-key [f10] 'call-last-kbd-macro)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup use-package
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; use-package git repo:
+;; https://github.com/jwiegley/use-package
+
+;; Install use-package if necessary
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Enable use-package
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ensure environment variables are the same in emacs as in shell
+(use-package exec-path-from-shell
+  :ensure t
+  :config (when (memq window-system '(mac ns))
+	    (exec-path-from-shell-initialize)))
+
+(use-package multiple-cursors
+  :ensure t
+  :bind ("C-c m c" . mc/edit-lines))
+    
+(use-package ace-jump-mode
+  :ensure t
+  :bind ("C-c SPC" . ace-jump-mode))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+ (use-package beacon
+  :ensure t
+  :config
+  (beacon-mode 1)
+  (setq beacon-color "#666600"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom Major Modes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; .handlebars files open in html
+(add-to-list 'auto-mode-alist '("\\.handlebars\\'" . html-mode))
+
+;; .ino files open in C
+(add-to-list 'auto-mode-alist '("\\.ino\\'" . c-mode))
+
+;; .launch files open in xml
+(add-to-list 'auto-mode-alist '("\\.launch\\'" . xml-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Miscellaneous
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Comment Line
+;; http://stackoverflow.com/a/9697222/3105650
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+(global-set-key (kbd "C-x C-;") 'comment-or-uncomment-region-or-line)
+
+;; rename buffer and file
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
+;; Author: Patrick Gundlach 
+;; nice mark - shows mark as a highlighted 'cursor' so user 'always' 
+;; sees where the mark is. Especially nice for killing a region.
+(defvar pg-mark-overlay nil
+  "Overlay to show the position where the mark is") 
+(make-variable-buffer-local 'pg-mark-overlay)
+(put 'pg-mark-mark 'face 'secondary-selection)
+(defvar pg-mark-old-position nil
+  "The position the mark was at. To be able to compare with the
+current position")
+(defun pg-show-mark () 
+  "Display an overlay where the mark is at. Should be hooked into 
+activate-mark-hook" 
+  (unless pg-mark-overlay 
+    (setq pg-mark-overlay (make-overlay 0 0))
+    (overlay-put pg-mark-overlay 'category 'pg-mark-mark))
+  (let ((here (mark t)))
+    (when here
+      (move-overlay pg-mark-overlay here (1+ here)))))
+(defadvice  exchange-point-and-mark (after pg-mark-exchange-point-and-mark)
+  "Show visual marker"
+  (pg-show-mark))
+(ad-activate 'exchange-point-and-mark)
+(add-hook 'activate-mark-hook 'pg-show-mark)
