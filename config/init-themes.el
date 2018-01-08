f29a1d23e51f962e82fe2a5d31e230b42b0e00d1
gerik <gerikzatorski@gmail.com>
2018-01-08 01:06:04 -0600

    focusing on linux

--- config/init-themes.el
+++ config/init-themes.el
@@ -3,11 +3,12 @@
   :init (progn (load-theme 'ample t t)
                (load-theme 'ample-flat t t)
                (load-theme 'ample-light t t))
+  :config (progn (enable-theme 'ample))
   :defer t)
 
 (use-package gruvbox-theme
   :ensure t
-  :init (progn (load-theme 'gruvbox-dark-medium t))
+  ;; :init (progn (load-theme 'gruvbox-dark-medium t))
   :defer t)
 
 (provide 'init-themes)
