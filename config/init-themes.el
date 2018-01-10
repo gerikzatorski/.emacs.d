8790e50c0ef9026609093be27c95495935bbf840
gerik <gerikzatorski@gmail.com>
2018-01-10 17:19:29 -0600

    commented out ample-theme section

--- config/init-themes.el
+++ config/init-themes.el
@@ -1,14 +1,14 @@
-(use-package ample-theme
-  :ensure t
-  :init (progn (load-theme 'ample t t)
-               (load-theme 'ample-flat t t)
-               (load-theme 'ample-light t t))
-  :config (progn (enable-theme 'ample))
-  :defer t)
+;; currently giving me hassle on multiple machines
+;; (use-package ample-theme
+;;   :ensure t
+;;   :init (progn (load-theme 'ample t t)
+;;                (load-theme 'ample-flat t t)
+;;                (load-theme 'ample-light t t))
+;;   :defer t)
 
 (use-package gruvbox-theme
   :ensure t
-  ;; :init (progn (load-theme 'gruvbox-dark-medium t))
+  :init (progn (load-theme 'gruvbox-dark-medium t))
   :defer t)
 
 (provide 'init-themes)
