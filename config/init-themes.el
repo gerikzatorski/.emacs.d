85b550a7f7fa0922203b6a2e530c2a4165eeeb71
gerik <gerikzatorski@gmail.com>
2018-01-10 21:30:47 -0600

    using submodules now for themes

--- config/init-themes.el
+++ config/init-themes.el
@@ -1,14 +1,11 @@
-;; currently giving me hassle on multiple machines
-;; (use-package ample-theme
-;;   :ensure t
-;;   :init (progn (load-theme 'ample t t)
-;;                (load-theme 'ample-flat t t)
-;;                (load-theme 'ample-light t t))
-;;   :defer t)
+;; add themes recursively from themes directory
+;; https://www.emacswiki.org/emacs/CustomThemes
+(let ((basedir "~/.emacs.d/themes/"))
+      (dolist (f (directory-files basedir))
+        (if (and (not (or (equal f ".") (equal f "..")))
+                 (file-directory-p (concat basedir f)))
+            (add-to-list 'custom-theme-load-path (concat basedir f)))))
 
-(use-package gruvbox-theme
-  :ensure t
-  :init (progn (load-theme 'gruvbox-dark-medium t))
-  :defer t)
+(load-theme 'material t)
 
 (provide 'init-themes)
