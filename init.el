880dd3bfbf7a6f329067c7c82a3fbaa552055c95
Gerik Zatorski <gerik.zatorski@luminartech.com>
2018-11-28 14:29:35 -0800

    must have packages and functions

--- init.el
+++ init.el
@@ -35,3 +35,52 @@
 (eval-when-compile
   (require 'use-package))
 (require 'bind-key)
+
+;; Packages...
+
+(use-package magit
+  :ensure t
+  :bind
+  ("C-x gs" . magit-status)
+  ("C-x gc" . magit-clone-url))
+
+(use-package ivy
+  :ensure t
+  :diminish
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t  ; add recent files and bookmarks to the ivy-switch-buffer
+        ivy-count-format "%d/%d ") ; ivy prompt formatting
+  :bind
+  ("C-c C-r" . ivy-resume))
+
+(use-package swiper
+  :ensure t
+  :bind
+  ("C-s" . swiper)  ; remapping isearch-forward
+  ("C-r" . swiper)) ; remapping isearch-backward
+
+(use-package counsel
+  :ensure t
+  :bind
+  ("M-x" . counsel-M-x)) ; remapping M-x
+
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  ("C-c m c" . mc/edit-lines)
+  ("C-c m n" . mc/insert-numbers)
+  ("C-c m l" . mc/insert-letters)
+  ("C-c m a" . mc/mark-all-like-this))
+
+(use-package expand-region
+  :ensure t
+  :bind
+  ("C-=" . er/expand-region)
+  ("C--" . er/contract-region))
+
+;; Functions (load all files in defuns-dir)
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+(dolist (file (directory-files defuns-dir t "\\w+"))
+  (when (file-regular-p file)
+    (load file)))
