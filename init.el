eb1aaf31014d3b56a8584dbc7aa99e31fde263ca
Gerik Zatorski <gerik.zatorski@luminartech.com>
2020-02-26 11:02:26 -0800

    add counsel-etags package

--- init.el
+++ init.el
@@ -166,6 +166,18 @@
   ("M-i" . counsel-imenu)
   ("M-y" . counsel-yank-pop))
 
+(use-package counsel-etags
+  :ensure t
+  :bind (("C-]" . counsel-etags-find-tag-at-point))
+  :init
+  (add-hook 'prog-mode-hook
+        (lambda ()
+          (add-hook 'after-save-hook
+            'counsel-etags-virtual-update-tags 'append 'local)))
+  :config
+  (setq counsel-etags-update-interval 60)
+  (push "build" counsel-etags-ignore-directories))
+
 (use-package avy
   :ensure t
   :bind
